28f7e8a324af16d62298702d658a35f5485eb986
KDY <rlaeodud9983@naver.com>
2021-12-09 16:54:08 +0900

    Initial commit

---
+++ app/src/main/java/org/gsm/software/wig/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package org.gsm.software.wig.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.gsm.software.wig.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
